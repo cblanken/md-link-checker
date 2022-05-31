bb33796a302308375b52c8b7f9b7a7e95fbe4eea
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-05-30 21:33:02 -0400

    Split each curl call into a new process for faster execution

--- md-link-checker.sh
+++ md-link-checker.sh
@@ -9,7 +9,7 @@ if [ -z "$links" ]; then
 fi
 
 # Output colors
-txtbnd=$(tput bold)
+txtbld=$(tput bold)
 bldred=${txtbld}$(tput setaf 1)
 bldgreen=${txtbld}$(tput setaf 2)
 bldyellow=${txtbld}$(tput setaf 3)
@@ -20,28 +20,37 @@ echo -e "Status\t| Link"
 echo -e "---------------"
 
 # Check each link
+set -a
 i=0; good_count=0; bad_count=0; redirect_count=0
-while read link; do
+
+function get_status_code() {
     status_code=$(curl -sw "%{http_code}" -o /dev/null "$link")
     status_code_category="${status_code:0:1}"
-    if [ "$status_code_category" -eq "1" ] || [ "$status_code_category" -eq "2" ]; then
-        echo -e "$status_code\t| $bldgreen$link$txtrst"
-        good_count=$((good_count+1))
-    elif [ "$status_code_category" -eq "3" ]; then
-        echo -e "$status_code\t| $bldyellow$link$txtrst"
-        redirect_count=$((good_count+1))
-    elif [ "$status_code_category" -eq "4" ] || [ "$status_code_category" -eq "4" ]; then
-        echo -e "$status_code\t| $bldred$link$txtrst"
-        bad_count=$((bad_count+1))
-    else
-        echo -e "$status_code\t| $link"
-    fi
+}
+
+while read -r link; do
+    {
+        status_code=$(curl -Lsw "%{http_code}" -o /dev/null "$link")
+        status_code_category="${status_code:0:1}"
+        if [ "$status_code_category" -eq "1" ] || [ "$status_code_category" -eq "2" ]; then
+            echo -e "$status_code\t| $bldgreen$link$txtrst"
+            good_count=$((good_count+1))
+        elif [ "$status_code_category" -eq "3" ]; then
+            echo -e "$status_code\t| $bldyellow$link$txtrst"
+            redirect_count=$((good_count+1))
+        elif [ "$status_code_category" -eq "4" ] || [ "$status_code_category" -eq "5" ]; then
+            echo -e "$status_code\t| $bldred$link$txtrst"
+            bad_count=$((bad_count+1))
+        else
+            echo -e "$status_code\t| $link"
+        fi
+    }& # Fork each execution to skip waiting for curl on each iteration
     i=$((i+1))
 done <<< "$links"
 
-echo -e "\nChecked $i links in $1"
-echo -e "$bldgreen$good_count$txtrst\tgood links"
-echo -e "$bldred$bad_count$txtrst\tbad links"
-echo -e "$bldyellow$redirect_count$txtrst\tredirects"
-echo ""
-
+wait
+echo -e "\nChecked $i links in $1\n"
+#echo -e "$bldgreen$good_count$txtrst\tgood links"
+#echo -e "$bldred$bad_count$txtrst\tbad links"
+#echo -e "$bldyellow$redirect_count$txtrst\tredirects"
+#echo ""
