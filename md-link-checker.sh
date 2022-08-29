b680bcfb5d5ccc2c5ca84713be772d408df9bb38
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-08-29 12:34:30 -0400

    Count HTTP statuses and display in table

--- md-link-checker.sh
+++ md-link-checker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -a
 
 # Usage
 if [ "$#" -ne 1 ]; then
@@ -6,7 +7,6 @@ if [ "$#" -ne 1 ]; then
     exit 0
 fi
 
-printf "\nChecking links in $1\n"
 links=$(grep -ioP "\(http?s://[^\s:,]+\)" "$1" | sed 's/(//g; s/)//g')
 
 # Exit if no links were found in markdown file
@@ -26,45 +26,66 @@ bldcyan=${txtbld}$(tput setaf 6)
 bldwhite=${txtbld}$(tput setaf 7)
 txtrst=$(tput sgr0)
 
-# Headers
+printf "\nChecking links in $1...\n"
+echo -e "---------------"
 echo -e "Status\t| Link"
 echo -e "---------------"
-
-# Check each link
-set -a
-function get_status_code() {
-    status_code=$(curl -sw "%{http_code}" -o /dev/null "$link")
-    status_code_category="${status_code:0:1}"
-}
 
 i=0;
 info_cnt=0; success_cnt=0; redirect_cnt=0; client_err_cnt=0; server_err_cnt=0; unknown_cnt=0;
 while read -r link; do
+    i=$((i+1))
     {
         status_code=$(curl -Lsw "%{http_code}" -o /dev/null "$link")
         status_code_category="${status_code:0:1}"
         if [ "$status_code_category" -eq "1" ]; then
             echo -e "$status_code\t| $bldwhite$link$txtrst"
             info_cnt=$((info_cnt+1))
+            exit 1
         elif [ "$status_code_category" -eq "2" ]; then
             echo -e "$status_code\t| $bldgreen$link$txtrst"
             success_cnt=$((success_cnt+1))
+            exit 2
         elif [ "$status_code_category" -eq "3" ]; then
             echo -e "$status_code\t| $bldyellow$link$txtrst"
             redirect_cnt=$((redirect_cnt+1))
+            exit 3
         elif [ "$status_code_category" -eq "4" ]; then
             echo -e "$status_code\t| $bldmagenta$link$txtrst"
             client_err_cnt=$((client_err_cnt+1))
+            exit 4
         elif [ "$status_code_category" -eq "5" ]; then
             echo -e "$status_code\t| $bldred$link$txtrst"
             server_err_cnt=$((server_err_cnt+1))
+            exit 5
         else
             echo -e "$status_code\t| $bldcyan$link$txtrst"
             unknown_cnt=$((unkwnown_cnt+1))
+            exit 6
         fi
     }& # Fork each execution to skip waiting for curl on each iteration
-    i=$((i+1))
+    pids[${i}]=$! # keep array of fork PIDs
 done <<< "$links"
+
+# Wait for each fork to complete and check HTTP status
+for pid in ${pids[@]}; do
+    wait "$pid"
+    ret=$?
+    if [ "$ret" == 1 ]; then
+        info_cnt=$((info_cnt+1))
+    elif [ "$ret" == 2 ]; then
+        success_cnt=$((success_cnt+1))
+    elif [ "$ret" == 3 ]; then
+        redirect_cnt=$((redirect_cnt+1))
+    elif [ "$ret" == 4 ]; then
+        client_err_cnt=$((client_err_cnt+1))
+    elif [ "$ret" == 5 ]; then
+        server_err_cnt=$((server_err_cnt+1))
+        echo "SUCCESS: $success_cnt"
+    elif [ "$ret" == 6 ]; then
+        unknown_cnt=$((unknown_cnt+1))
+    fi
+done
 
 wait # wait for all forks to complete
 
@@ -77,5 +98,5 @@ printf "${bldgreen}2xx\t| ${success_cnt}${txtrst}\n"
 printf "${bldyellow}3xx\t| ${redirect_cnt}${txtrst}\n"
 printf "${bldmagenta}4xx\t| ${client_err_cnt}${txtrst}\n"
 printf "${bldred}5xx\t| ${server_err_cnt}${txtrst}\n"
-printf "${bldcyan}UNKNOWN\t| ${unknown_cnt}${txtrst} => Curl probably recieved 000 and may have timed out\n"
+printf "${bldcyan}UNKNOWN\t| ${unknown_cnt}${txtrst} => Curl probably received 000 and may have timed out\n"
 printf "\n"
